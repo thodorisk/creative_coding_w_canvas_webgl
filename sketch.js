34d752879599b31406a5d5f96a7af66b271ae35b
thodoris <thodorisk@icloud.com>
2021-08-24 17:11:10 +0300

    linear interpolation (adding margins)

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 const canvasSketch = require('canvas-sketch');
+const { lerp } = require('canvas-sketch-util/math'); //function for linear interpolation
 
 const settings = {
   dimensions: [2048, 2048]
@@ -8,6 +9,7 @@ const sketch = () => {
   const createGrid = () => {
     const points = [];
     const count = 5;
+
     for (let x = 0; x<count; x++) {
       for(let y = 0; y<count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
@@ -20,15 +22,15 @@ const sketch = () => {
   }
 
   const points = createGrid();
-  console.log(points);
+  const margin = 200;
 
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0,0,width,height);
 
     points.forEach(([u, v]) => {
-      const x = u * width;
-      const y = v * height;
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, height - margin, v);
 
       context.beginPath();
       context.arc(x, y, 100, 0, Math.PI * 2, false)
