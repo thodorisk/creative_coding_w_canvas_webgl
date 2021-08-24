8311e1e68916f6c4eec534dccd7b97173614fe3f
thodoris <thodorisk@icloud.com>
2021-08-24 17:04:44 +0300

    drawing the grid

--- sketch.js
+++ sketch.js
@@ -1,27 +1,39 @@
 const canvasSketch = require('canvas-sketch');
 
-// the purpose of canvas sketch ??? Code works across the web but also print.
-// ctrl + s after focusing on the browser window. exports an image.
-
 const settings = {
-  dimensions: 'A4',
-  //orientation: 'landscape',
-  pixelsPerInch: 300, // printing reasons,
-  units: 'cm' //units will be in cm instead of px.
+  dimensions: [2048, 2048]
 };
 
 const sketch = () => {
+  const createGrid = () => {
+    const points = [];
+    const count = 5;
+    for (let x = 0; x<count; x++) {
+      for(let y = 0; y<count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+        points.push([u, v]);
+      }
+    }
+
+    return points;
+  }
+
+  const points = createGrid();
+  console.log(points);
+
   return ({ context, width, height }) => {
-    context.fillStyle = 'red';
-    context.fillRect(0, 0, width, height);
+    context.fillStyle = 'white';
+    context.fillRect(0,0,width,height);
 
-    context.beginPath();
-    context.arc(width / 2, height / 2, width * 0.2, 0, Math.PI * 2, false); //  canvas wep api basics: CanvasRenderingContext2D.arc()
-    context.fillStyle = 'blue';
-    context.fill();
+    points.forEach(([u, v]) => {
+      const x = u * width;
+      const y = v * height;
 
-    context.lineWidth = width * 0.05;
-    context.stroke();
+      context.beginPath();
+      context.arc(x, y, 100, 0, Math.PI * 2, false)
+      context.stroke();
+    })
   };
 };
 
