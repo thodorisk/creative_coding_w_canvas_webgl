ed97ae15d436c95aedcbd30bdce8dfd4dad5f7fe
thodoris <thodorisk@icloud.com>
2021-08-24 20:22:50 +0300

    adding organic randomness

--- sketch.js
+++ sketch.js
@@ -15,7 +15,12 @@ const sketch = () => {
       for(let y = 0; y<count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
-        points.push([u, v]);
+
+        // give individual radius to circles
+        points.push({
+          radius: Math.abs(random.gaussian() * 0.01), // random.value() ends up with equal distribution of numbers thats why i use gaussian to produce more organic randomness.
+          position: [u, v]
+        });
       }
     }
 
@@ -30,14 +35,21 @@ const sketch = () => {
     context.fillStyle = 'white';
     context.fillRect(0,0,width,height);
 
-    points.forEach(([u, v]) => {
+    points.forEach(data => {
+      const {
+        position,
+        radius
+      } = data;
+
+      const [u, v] = position;
+
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
       context.beginPath();
-      context.arc(x, y, 5, 0, Math.PI * 2, false);
-      context.lineWidth = 10;
-      context.stroke();
+      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      context.fillStyle = 'red';
+      context.fill();
     })
   };
 };
