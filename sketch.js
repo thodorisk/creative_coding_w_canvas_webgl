6f1207c9d89432dace623f2e1927d23ac00b60e3
thodoris <thodorisk@icloud.com>
2021-08-24 14:24:42 +0300

    canvas api basics

--- sketch.js
+++ sketch.js
@@ -8,6 +8,14 @@ const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'red';
     context.fillRect(0, 0, width, height);
+
+    context.beginPath();
+    context.arc(width / 2, height / 2, 200, 0, Math.PI * 2, false); //  canvas wep api basics: CanvasRenderingContext2D.arc()
+    context.fillStyle = 'blue';
+    context.fill();
+
+    context.lineWidth = 10;
+    context.stroke();
   };
 };
 
