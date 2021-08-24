46945b087b7c9d6f753cbb64570867e8dcb885af
thodoris <thodorisk@icloud.com>
2021-08-24 17:40:20 +0300

    adding randomness

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 const canvasSketch = require('canvas-sketch');
 const { lerp } = require('canvas-sketch-util/math'); //function for linear interpolation
+const random = require('canvas-sketch-util/random'); //deterministic randomness
 
 const settings = {
   dimensions: [2048, 2048]
@@ -8,7 +9,7 @@ const settings = {
 const sketch = () => {
   const createGrid = () => {
     const points = [];
-    const count = 5;
+    const count = 40;
 
     for (let x = 0; x<count; x++) {
       for(let y = 0; y<count; y++) {
@@ -21,7 +22,8 @@ const sketch = () => {
     return points;
   }
 
-  const points = createGrid();
+  random.setSeed(512);
+  const points = createGrid().filter(() => random.value() > 0.5); //Math.random() > 0.5);
   const margin = 200;
 
   return ({ context, width, height }) => {
@@ -33,7 +35,8 @@ const sketch = () => {
       const y = lerp(margin, height - margin, v);
 
       context.beginPath();
-      context.arc(x, y, 100, 0, Math.PI * 2, false)
+      context.arc(x, y, 5, 0, Math.PI * 2, false);
+      context.lineWidth = 10;
       context.stroke();
     })
   };
