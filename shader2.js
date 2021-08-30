ac3a9685ff943000e7e71d841d8e990d5cda077d
thodoris <thodorisk@icloud.com>
2021-08-30 19:33:25 +0300

    texture coordinates

--- shader2.js
+++ shader2.js
@@ -48,16 +48,18 @@ const sketch = ({ context }) => {
   const fragmentShader = /* glsl */`
     varying vec2 vUv;
     uniform vec3 color;
+    uniform float time;
 
     void main () {
-      gl_FragColor = vec4(vec3(vUv.x) * color, 1.0);
+      gl_FragColor = vec4(vec3(vUv.x + sin(time), vUv.x, vUv.y + cos(time)) * color, 1.0);
     }
   `;
 
   // Setup a material
   const material = new THREE.ShaderMaterial({
     uniforms: {
-      color: {value: new THREE.Color("#ff00ff")}
+      time: {value: 0},
+      color: {value: new THREE.Color("#fff")}
     },
     vertexShader,
     fragmentShader
@@ -78,6 +80,7 @@ const sketch = ({ context }) => {
     },
     // Update & render your scene here
     render({ time }) {
+      material.uniforms.time.value = time;
       controls.update();
       renderer.render(scene, camera);
     },
